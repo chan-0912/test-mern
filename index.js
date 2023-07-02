d1c3b5c652dc392c33042e04e6afe497519c1c00
chan-0912 <yadavchanchal679@gmail.com>
2023-07-03 01:35:22 +0530

    cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://cosmic-sorbet-bc3061.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
